73e2d1ee7e46cc4bc980d4aeba04c03645ea4669
Domingos Muratori <domingosmuratori@outlook.com>
2025-11-01 13:55:13 +0000

    correc.

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let altura = Number(prompt("Digite a sua altura: "));
 let imc = peso / (altura * altura);
 
 if (imc < 18) {
-  alert("Cuidado, você está abaixo do peso ideal!")
+  alert(`Seu IMC é ${imc}. Cuidado, você está abaixo do peso ideal!`)
 } else if (imc >= 18 && imc <= 25) {
-  alert("Parabéns, você está no peso ideal!")
+  alert(`Seu IMC é ${imc}. Parabéns, você está no peso ideal!`)
 } else if (imc >= 25 && imc <= 30) {
-  alert("Cuidado, você está no sobrepeso!")
+  alert(`Seu IMC é ${imc}. Cuidado, você está no sobrepeso!`)
 } else if (imc >= 30 && imc <= 35) {
-  alert("Cuidado, você está com obesidade moderada!")
+  alert(`Seu IMC é ${imc}. Cuidado, você está com obesidade moderada!`)
 } else if (imc >= 35 && imc <= 40) {
-  alert("Cuidado, você está com obesidade grave!")
+  alert(`Seu IMC é ${imc}. Cuidado, você está com obesidade grave!`)
 } else {
-  alert("Cuidado, você está com obesidade mórbida!!")
+  alert(`Seu IMC é ${imc}. Cuidado, você está com obesidade mórbida!!`)
 }
